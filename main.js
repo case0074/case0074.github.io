ec7642eee60cf161ef3953dbadc7aadf654d5dde
Chandler Case <143656620+case0074@users.noreply.github.com>
2024-09-26 01:45:02 -0500

    yur

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*= 
 */
 import './style.css';
-import * as THREE from './three';
-import { OrbitControls } from './three/addons/controls/OrbitControls.js';
+import * as THREE from '/three';
+import { OrbitControls } from '/three/addons/controls/OrbitControls.js';
 
 
 console.log(`                                                       
