3058ebb5a18672f6e79cbe226c8fad2eb5d1b7bd
Chandler Case <143656620+case0074@users.noreply.github.com>
2024-10-01 17:26:12 -0500

    hehe

--- main.js
+++ main.js
@@ -6,8 +6,9 @@
 /*
 i want this to work more than nything sos */
 import './style.css'
-import * as THREE from './src/three.cjs';
-import { OrbitControls } from './src/OrbitControls.js';
+  import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.168.0/build/three.module.js';
+  import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.168.0/examples/jsm/controls/OrbitControls.js';
+
 
 console.log(`                                                       
 m      #                                     "         
