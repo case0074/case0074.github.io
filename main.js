31c1ea648832449625de2110a3ae0111bdc4b83e
Chandler Case <143656620+case0074@users.noreply.github.com>
2024-10-01 11:16:13 -0500

    work plz

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
         Imports
 =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*= 
 */
-import './style.css';
+
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.168.0/build/three.module.min.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.168.0/examples/jsm/controls/OrbitControls.js';
 
